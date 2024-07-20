049df55bc675ed468730ca714816e0be7f4b4448
Chris <cristian.28hedz@gmail.com>
2024-07-19 18:50:05 -0600

    feat: agregado dto para la asistencia del empleado junto con su validacion

--- src/main/java/com/calva/controlasistencia/modelos/dto/AsistenciaDto.java
+++ src/main/java/com/calva/controlasistencia/modelos/dto/AsistenciaDto.java
@@ -1,8 +1,14 @@
 package com.calva.controlasistencia.modelos.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AsistenciaDto {
-    private Long idEmpleado;
+    @NotBlank(message = "El id del empleado es requerido")
+    @Size(min = 1, max = 3, message = "El id del empleado debe tener entre 1 y 3 caracteres")
+    @Pattern(regexp = "^[0-9]*$", message = "El id del empleado debe ser numérico")
+    private String idEmpleado;
 }
